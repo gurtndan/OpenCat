37da07078ad03f51bcce328beff05dff3fa382eb
gurtndan <daniel.gurtner93@gmail.com>
2020-07-02 00:23:09 +0200

    Inverse Kinematic functions

--- ArduinoCode/WalkRobot.ino
+++ ArduinoCode/WalkRobot.ino
@@ -16,8 +16,11 @@
   Written by Limor Fried/Ladyada for Adafruit Industries.  
   BSD license, all text above must be included in any redistribution
  ****************************************************/
+ 
 
-// RECIVER
+/****************************************************
+ * DECLARATION RECIVIER 
+ ****************************************************/
 //#include <SPI.h>
   #include <nRF24L01.h>
   #include <RF24.h>
@@ -27,6 +30,7 @@
   const byte address[6] = "00001";
   unsigned long lastReceiveTime = 0;
   unsigned long currentTime = 0;
+  
   // Max size of this struct is 32 bytes
   struct Data_Package {
     byte j1PotX;
@@ -45,9 +49,10 @@
     byte button4;
   };
   Data_Package data; //Create a variable with the above structure
-// END RECIVER
 
-
+/****************************************************
+ * DECLARATION PWM SERVODRIVER
+ ****************************************************/
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
@@ -60,15 +65,42 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define USMAX  2400 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
 #define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
 uint8_t servonum = 0;
-int degree[16];// = {0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0};
 
-float a = 0;
-float Z = 0;
+/****************************************************
+ * DECLARATION INTERNAL GLOBAL VAR
+ ****************************************************/
+int CalibAngle = 100;
+int Calibnumber = 0;
+int Calibration[16] = {310,270,320,300, 350,350,310,310, 290,300,300,290, 310,280,310,290};
 
+struct Orientation {
+    float Xaxis;
+    float Yaxis;
+    float Zaxis;
+};
+Orientation RobotPosition; //Create a variable with the above structure
 
+class  Legpos {
+    // Access specifier 
+    public: 
+    // Data Members 
+    float HipAngle;
+    float ShoulderAngle;
+    float UpperlegAngle;
+    float LowerlegAngle;
+};
+
+Legpos LeftFrontleg;  //Create a variable with the above structure
+Legpos LeftBackleg;   //Create a variable with the above structure
+Legpos RightFrontleg; //Create a variable with the above structure
+Legpos RightBackleg;  //Create a variable with the above structure
+
+/****************************************************
+ * CODE SETUP
+ ****************************************************/
 void setup() {
   Serial.begin(9600);
-  Serial.println("8 channel Servo test!");
+  Serial.println("Start setup code");
 
   pwm.begin();
   pwm.setOscillatorFrequency(27000000);
@@ -84,132 +116,257 @@ void setup() {
   radio.setPALevel(RF24_PA_LOW);
   radio.startListening(); //  Set the module as receiver
   resetData();
-
+  
+  Serial.println("Start execution code");
 }
 
-
+/****************************************************
+ * CODE MAIN LOOP
+ ****************************************************/
 void loop() {
-  // RECIVER
-  // Check whether we keep receving data, or we have a connection between the two modules
-  currentTime = millis();
-  if ( currentTime - lastReceiveTime > 1000 ) { // If current time is more then 1 second since we have recived the last data, that means we have lost connection
-    resetData(); // If connection is lost, reset the data. It prevents unwanted behavior, for example if a drone jas a throttle up, if we lose connection it can keep flying away if we dont reset the function
-  }
-  // Check whether there is data to be received
-  if (radio.available()) {
-    radio.read(&data, sizeof(Data_Package)); // Read the whole data and store it into the 'data' structure
-    lastReceiveTime = millis(); // At this moment we have received the data
-  }
-
-  // Print Result
+  // --- MANUAL CALIBRATION
+  //ManualCalibration();
   
-  Serial.print("Joystick 1X: ");
-  Serial.print(data.j1PotX);
-  Serial.print(" Joystick 1Y: ");
-  Serial.println(data.j1PotY);
-  /*
-  Serial.print(" Joystick 2X: ");
-  Serial.print(data.j2PotX);
-  Serial.print(" Joystick 2Y: ");
-  Serial.print(data.j2PotY);
+  // --- RC COMMAND ---
+  rcReciver(0);
+  //  RobotPosition.Zaxis = map(data.j1PotX,0,255, 0, 90);
 
-  Serial.print(" Button 1: ");
-  Serial.print(data.button1);
-  Serial.print(" Button 2: ");
-  Serial.print(data.button2);
-  Serial.print(" Button 3: ");
-  Serial.print(data.button3);
-  Serial.print(" Button 4: ");
-  Serial.print(data.button4);
-
-  Serial.print(" Pot 1: ");
-  Serial.print(data.pot1);
-  Serial.print(" Pot 2: ");
-  Serial.println(data.pot2);
-  
-  */
-
-
-  // HC-05 Blutooth
-  if(Serial.available()>0){
-  int i;
-  char data = Serial.read();
-    switch(data){
-      case 'a':
-            for (i=0; i<11;i++){
-              Z++;
-              }
-              break;
-  
-  
-      case 'c':
-      for (i=0; i<11;i++){
-              Z--;
-              }
-                break;
-  
-      
-    }
-  }
-
-
+  // --- BLUETOOTH COMMAND ---
+  //RobotPosition = bluetooth(RobotPosition);
+  bluetooth(RobotPosition);
+  // --- MAIN ---
+  CalcLeg(RobotPosition);
+  ServoControl();
 
   
-  //Serial.println(data.j1PotX);
-  // Z = map(data.j1PotX,0,255, 0, 90);
-  Z = 50;
-
-
-  // MAIN
-  calcLeg();
-
-  // command Stand
-  // Vorne Rechts
-  degree[0] = a;
-  degree[1] = -a;
-  degree[2] = 0;
-  degree[3] = 0;
-  
-  // Hinten Rechts
-  degree[4] = -a;
-  degree[5] = a;
-  degree[6] = 0;
-  degree[7] = 0;
- 
-  // Hinten Links
-  degree[8] = a;
-  degree[9] = -a;
-  degree[10] = 0;
-  degree[11] = 0;
-
-  // Vorne Links
-  degree[12] = -a;
-  degree[13] = a;
-  degree[14] = 0;
-  degree[15] = 0;
-          
-
-  
-  servo();
-
 
   delay(5);
 }
 
 
 
-void calcLeg(){
+
+/****************************************************
+ * CODE DATA BLUETOOTH HC-05
+ ****************************************************/
+// Acces adress of input and manipulate it
+void bluetooth(Orientation &Value){
+  // --- HC-05 Blutooth ---
+
+  //Orientation &Value
+  char data = Serial.read();
+
+  //if(Serial.available()>0){
+    int i;
+      switch(data){
+        case 'a':
+                Value.Zaxis += 10;
+                break;
+    
+    
+        case 'b':
+                Value.Zaxis -= 10;
+                break;
+
+        case 'c':
+                Value.Xaxis += 10;
+                break;
+    
+        case 'd':
+                Value.Xaxis -= 10;
+                break;
+
+        case 'e':
+                Value.Yaxis += 10;
+                break;
+
+        case 'f':
+                Value.Yaxis -= 10;
+                break;
+      }
+  //}
+
+  // Xaxis,Yaxis,Zaxis
+  /*
+  Serial.print("ZAxis Command : ");
+  Serial.print(RobotPosition.Zaxis);
+  Serial.print(" YAxis Command : ");
+  Serial.print(RobotPosition.Yaxis);
+  Serial.print(" XAxis Command : ");
+  Serial.println(RobotPosition.Xaxis);
+ */
+  //return {Value} ;
+}
+
+
+
+/****************************************************
+ * CODE RESET DATA RC-TRANSMITTER
+ ****************************************************/
+void rcReciver(bool printResult){
+    // --- RECIVER ---
+    // Check whether we keep receving data, or we have a connection between the two modules
+    currentTime = millis();
+    if ( currentTime - lastReceiveTime > 1000 ) { // If current time is more then 1 second since we have recived the last data, that means we have lost connection
+      resetData(); // If connection is lost, reset the data. It prevents unwanted behavior, for example if a drone jas a throttle up, if we lose connection it can keep flying away if we dont reset the function
+    }
+    // Check whether there is data to be received
+    if (radio.available()) {
+      radio.read(&data, sizeof(Data_Package)); // Read the whole data and store it into the 'data' structure
+      lastReceiveTime = millis(); // At this moment we have received the data
+    }
   
-  int L0 = 45;
-  int L1 = 45;
-  int S1 = 23;
-  int H1 = 23;
-  a = acos((Z/2)/L0)*(180/3.14);
+    // --- Print Result ---
+    if (printResult == 1){
+      Serial.print("Joystick 1X: ");
+      Serial.print(data.j1PotX);
+      Serial.print(" Joystick 1Y: ");
+      Serial.println(data.j1PotY);
+      Serial.print(" Joystick 2X: ");
+      Serial.print(data.j2PotX);
+      Serial.print(" Joystick 2Y: ");
+      Serial.print(data.j2PotY);
+    
+      Serial.print(" Button 1: ");
+      Serial.print(data.button1);
+      Serial.print(" Button 2: ");
+      Serial.print(data.button2);
+      Serial.print(" Button 3: ");
+      Serial.print(data.button3);
+      Serial.print(" Button 4: ");
+      Serial.print(data.button4);
+    
+      Serial.print(" Pot 1: ");
+      Serial.print(data.pot1);
+      Serial.print(" Pot 2: ");
+      Serial.println(data.pot2);
+    }
+ }
+
+/****************************************************
+ * CODE RESET DATA RC-TRANSMITTER
+ ****************************************************/
+void resetData() {
+  // Reset the values when there is no radio connection - Set initial default values
+  data.j1PotX = 127;
+  data.j1PotY = 127;
+  data.j2PotX = 127;
+  data.j2PotY = 127;
+  data.j1Button = 1;
+  data.j2Button = 1;
+  data.pot1 = 1;
+  data.pot2 = 1;
+  data.tSwitch1 = 1;
+  data.tSwitch2 = 1;
+  data.button1 = 1;
+  data.button2 = 1;
+  data.button3 = 1;
+  data.button4 = 1;
+}
+
+/****************************************************
+ * CODE CALCULATION MOTOR POSITIONS
+ ****************************************************/
+void CalcLeg(Orientation Value){
+  float AngleUp;
+  float AngleLo;
+  float AngleUp2;
+  float Zaxis2;
+  float AngleHip;
+  float LegnthM;
+  float Zaxis3;
+
+  float AngleUp_1;
+  float AngleLo_Deg;
+  float AngleUp_Deg;
+  float AngleHip_Deg;
+  
+  // --- INVERSE KINEMATIC
+  float L0 = 45;  // Lowerleg length
+  float L1 = 45;  // Upperleg length
+  float S1 = 30;  // Shoulder length
+  float H1 = 23;  // Hip length
+
+
+  // Leg in Y
+  LegnthM = sqrt(sq(Value.Zaxis)+sq(Value.Yaxis));
+  Zaxis3 = sqrt(sq(S1)+sq(LegnthM));
+  AngleHip = atan(Value.Yaxis/Value.Zaxis);
+  
+  // Leg in X
+  AngleUp2 = atan((Value.Xaxis/Zaxis3));
+  Zaxis2 = Zaxis3 / cos(AngleUp2);
+  
+  // Leg in Z in DEG
+  AngleUp_1 = acos(((-0.5)*sq(L1)+0.5*sq(L0)+0.5*sq(Zaxis2))/(L0*Zaxis2)); // Same result = for unefen length L0, L1
+
+  //AngleUp_1 = acos((Zaxis2/2)/L0); // Alternativ result
+
+  AngleUp = AngleUp_1+AngleUp2;
+  AngleLo = (2*AngleUp_1);
+
+
+  // DEGREES
+  AngleLo_Deg = AngleLo*(180/PI);
+  AngleUp_Deg = AngleUp*(180/PI);
+  AngleHip_Deg = AngleHip*(180/PI);
+
+  
+  ::RightFrontleg.HipAngle = AngleHip_Deg;
+  ::RightFrontleg.ShoulderAngle = 0;
+  ::RightFrontleg.UpperlegAngle = AngleUp_Deg;
+  ::RightFrontleg.LowerlegAngle = (-1)*AngleLo_Deg;
+
+  
+  Serial.print(" ZAxis Command : ");
+  Serial.print(RobotPosition.Zaxis);
+  Serial.print(" YAxis Command : ");
+  Serial.print(RobotPosition.Yaxis);
+  Serial.print(" XAxis Command : ");
+  Serial.println(RobotPosition.Xaxis);
  
 
+  ::RightBackleg.HipAngle = AngleHip_Deg;
+  ::RightBackleg.ShoulderAngle = 0;
+  ::RightBackleg.UpperlegAngle = AngleUp_Deg;
+  ::RightBackleg.LowerlegAngle = (-1)*AngleLo_Deg;
 
+  
+  ::LeftFrontleg.HipAngle =  (-1)*AngleHip_Deg;
+  ::LeftFrontleg.ShoulderAngle = 0;
+  ::LeftFrontleg.UpperlegAngle = AngleUp_Deg;
+  ::LeftFrontleg.LowerlegAngle = (-1)*AngleLo_Deg;
+
+  ::LeftBackleg.HipAngle =  (-1)*AngleHip_Deg;
+  ::LeftBackleg.ShoulderAngle = 0;
+  ::LeftBackleg.UpperlegAngle = AngleUp_Deg;
+  ::LeftBackleg.LowerlegAngle = (-1)*AngleLo_Deg;
+
+/*
+
+   ::LeftFrontleg.HipAngle = 15;
+  ::LeftFrontleg.ShoulderAngle = 15;
+  ::LeftFrontleg.UpperlegAngle = 15;
+  ::LeftFrontleg.LowerlegAngle = 15;
+
+  ::LeftBackleg.HipAngle = 15;
+  ::LeftBackleg.ShoulderAngle = 15;
+  ::LeftBackleg.UpperlegAngle = 15;
+  ::LeftBackleg.LowerlegAngle = 15;
+
+  ::RightFrontleg.HipAngle = 0;
+  ::RightFrontleg.ShoulderAngle = 15;
+  ::RightFrontleg.UpperlegAngle = 15;
+  ::RightFrontleg.LowerlegAngle = 15;
+
+  ::RightBackleg.HipAngle = 15;
+  ::RightBackleg.ShoulderAngle = 15;
+  ::RightBackleg.UpperlegAngle = 15;
+  ::RightBackleg.LowerlegAngle = 15;
+*/
 
  /*
+  --- FORWARD KINEMATIC ---
   
   int a0 = degree[0];
   int a1 = degree[1];
@@ -252,75 +409,97 @@ void calcLeg(){
   X = XHip-11;
   Y = YHip-48;
   */
-  
-  }
-
-
-
- 
-void resetData() {
-  // Reset the values when there is no radio connection - Set initial default values
-  data.j1PotX = 127;
-  data.j1PotY = 127;
-  data.j2PotX = 127;
-  data.j2PotY = 127;
-  data.j1Button = 1;
-  data.j2Button = 1;
-  data.pot1 = 1;
-  data.pot2 = 1;
-  data.tSwitch1 = 1;
-  data.tSwitch2 = 1;
-  data.button1 = 1;
-  data.button2 = 1;
-  data.button3 = 1;
-  data.button4 = 1;
 }
 
-
- void servo() {
-  // put your main code here, to run repeatedly://100
-
-  // Vorne Rechts
-  int pulse0 = map(degree[0], -90,90, 510, 100);
-  int pulse1 = map(degree[1], -90,90, 510, 100); 
-  int pulse2 = map(degree[2], -90,90, 100, 510);
-  int pulse3 = map(degree[3], -90,90, 100, 510);
+/****************************************************
+ * CODE DRIVE SERVOS
+ * 
+ * Maps command in degrees to calibrated motors as an
+ * pwm output
+ ****************************************************/
+ void ServoControl() {
+  // --- PCA9685 ---
+  int Limit = 200;
+  
+  // FRONT RIGHT LEG
+  int pulse0 = map(RightFrontleg.LowerlegAngle, -90,90, Calibration[0]-Limit,Calibration[0]+Limit);  // Lower leg
+  int pulse1 = map(RightFrontleg.UpperlegAngle, -90,90, Calibration[1]-Limit,Calibration[1]+Limit);  // Upper leg
+  int pulse2 = map(RightFrontleg.ShoulderAngle, -90,90, Calibration[2]+Limit,Calibration[2]-Limit);  // Soulder
+  int pulse3 = map(RightFrontleg.HipAngle, -90,90, Calibration[3]+Limit,Calibration[3]-Limit);  // Hip
    
   pwm.setPWM(0,0,pulse0);
   pwm.setPWM(1,0,pulse1);
   pwm.setPWM(2,0,pulse2);
   pwm.setPWM(3,0,pulse3);
   
-  // Hinten Rechts
-  int pulse4 = map(degree[4], -90,90, 100, 510);
-  int pulse5 = map(degree[5], -90,90, 100, 510); 
-  int pulse6 = map(degree[6], -90,90, 510, 100);
-  int pulse7 = map(degree[7], -90,90, 510, 100);
+  // BACK RIGHT LEG
+  int pulse4 = map(RightBackleg.LowerlegAngle, -90,90, Calibration[4]-Limit,Calibration[4]+Limit);  // Lower leg
+  int pulse5 = map(RightBackleg.UpperlegAngle, -90,90, Calibration[5]-Limit,Calibration[5]+Limit);  // Upper leg
+  int pulse6 = map(RightBackleg.ShoulderAngle, -90,90, Calibration[6]-Limit,Calibration[6]+Limit);  // Soulder
+  int pulse7 = map(RightBackleg.HipAngle, -90,90, Calibration[7]-Limit,Calibration[7]+Limit);  // Hip
 
   pwm.setPWM(4,0,pulse4);
   pwm.setPWM(5,0,pulse5);
   pwm.setPWM(6,0,pulse6);
   pwm.setPWM(7,0,pulse7);
   
- // Hinten Links
-  int pulse8 = map(degree[8], -90,90, 100, 510);
-  int pulse9 = map(degree[9], -90,90, 100, 510); 
-  int pulse10 = map(degree[10], -90,90, 510, 100);
-  int pulse11 = map(degree[11], -90,90, 510, 100);
+ // BACK LEFT LEG
+  int pulse8 = map(LeftBackleg.LowerlegAngle, -90,90, Calibration[8]+Limit,Calibration[8]-Limit);    // Lower leg
+  int pulse9 = map(LeftBackleg.UpperlegAngle, -90,90, Calibration[9]+Limit,Calibration[9]-Limit);    // Upper leg 
+  int pulse10 = map(LeftBackleg.ShoulderAngle, -90,90, Calibration[10]-Limit,Calibration[10]+Limit);   // Soulder
+  int pulse11 = map(LeftBackleg.HipAngle, -90,90, Calibration[11]+Limit,Calibration[11]-Limit);  // Hip
    
   pwm.setPWM(8,0,pulse8);
   pwm.setPWM(9,0,pulse9);
   pwm.setPWM(10,0,pulse10);
   pwm.setPWM(11,0,pulse11);
   
-  // Vorne Links
-  int pulse12 = map(degree[12], -90,90, 510, 100);
-  int pulse13 = map(degree[13], -90,90, 510, 100); 
-  int pulse14 = map(degree[14], -90,90, 100, 510);
-  int pulse15 = map(degree[15], -90,90, 50, 510);
+  // FRONT LEFT LEG
+  int pulse12 = map(LeftFrontleg.LowerlegAngle, -90,90, Calibration[12]+Limit,Calibration[12]-Limit);  // Lower leg
+  int pulse13 = map(LeftFrontleg.UpperlegAngle, -90,90, Calibration[13]+Limit,Calibration[13]-Limit);  // Upper leg
+  int pulse14 = map(LeftFrontleg.ShoulderAngle, -90,90, Calibration[14]-Limit,Calibration[14]+Limit);  // Soulder
+  int pulse15 = map(LeftFrontleg.HipAngle, -90,90, Calibration[15]-Limit,Calibration[15]+Limit);   // Hip
   
   pwm.setPWM(12,0,pulse12);
   pwm.setPWM(13,0,pulse13);
   pwm.setPWM(14,0,pulse14);
   pwm.setPWM(15,0,pulse15);
+
+
+ // TESTS
+ uint8_t test1;
+  test1 = pwm.getPWM(15);
+
+  //pwm.wakeup();
+  //pwm.sleep();
+}
+
+
+/****************************************************
+ * CODE DATA BLUETOOTH HC-05
+ ****************************************************/
+// Acces adress of input and manipulate it
+void ManualCalibration(){
+  // --- HC-05 Blutooth ---
+  char data = Serial.read();
+      switch(data){
+        case 'a':
+                CalibAngle += 10;
+                break;
+        case 'c':
+                CalibAngle -= 10;
+                break;
+        case 'b':
+                Calibnumber += 1;
+                break;
+        case 'd':
+                Calibnumber -= 1;
+                break;
+        }
+        Serial.print("  Selected Servo ");
+        Serial.print(Calibnumber);
+        Serial.print("  Active PWM  ");
+        Serial.println(CalibAngle);
+        delay(50);
+        pwm.setPWM(Calibnumber,0,CalibAngle);
 }
